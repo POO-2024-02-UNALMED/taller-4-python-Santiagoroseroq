72397f40fdf34a934f9b0a7452dad69c9bd4e328
Santiagoroseroq <sroseroq@unal.edu.co>
2024-11-26 00:25:33 -0500

    Update grupo.py

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -3,30 +3,37 @@ from classroom.asignatura import Asignatura
 class Grupo:
     grado = None
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes):
+    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes=None):
+        if asignaturas is None:
+            asignaturas = []
+        if estudiantes is None:
+            estudiantes = []
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
-    def listadoAsignaturas(self, kwargs):
-        for x in kwargs.values():
-            self._asignaturas.append(Asignatura(x))
+    def listadoAsignaturas(self, *args):
+        for x in args:
+            self._asignaturas.append(Asignatura(x, "Remoto"))
 
-    def agregarAlumno(self, alumno, lista=[]):
+    def agregarAlumno(self, alumno, lista=None):
+        if lista is None:
+            lista = []
         lista.append(alumno)
         self.listadoAlumnos = self.listadoAlumnos + lista
 
-    # def __str__(self):
-    #     pass
-
-    @ classmethod
+    @classmethod
     def asignarNombre(cls, nombre="Grado 10"):
         cls.grado = nombre
 
-    @ classmethod
+    @classmethod
     def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
+    @classmethod
+    def asignarNombre(cls, nombre="Grado 12"):
         cls.grado = nombre
+
+    def __str__(self):
+        return f"Grupo de estudiantes: {self._grupo}\nGrado {self.grado}\n{self.listadoAlumnos}"
+
